948f429891afd689e788523766b0e75cc9b68d4a
rderbier <rderbier@gmail.com>
2023-03-29 18:40:53 -0700

    comments

--- sample/donors/convert.sh
+++ sample/donors/convert.sh
@@ -1,6 +1,10 @@
 # This is my first bash script
 echo "Converting Donors file"
-rm donors.rdf
+# clean file and copy schema - schema may be updated by the conversion
+rm donors.rdf 
+rm donors.schema
+cp schemas/donors.schema .
+# convert all csv in same rdf file
 ../../bin/csvtordf -f=/Users/raph/Rwork/Schools-CA10.csv -t=./templates/SchoolTemplate.txt -o=donors.rdf -s=donors.schema
 ../../bin/csvtordf -f=/Users/raph/Rwork/Projects-CA10.csv -t=./templates/ProjectTemplate.txt -o=donors.rdf -s=donors.schema
 ../../bin/csvtordf -f=/Users/raph/Rwork/Donors-CA10.csv -t=./templates/DonorTemplate.txt -o=donors.rdf -s=donors.schema
